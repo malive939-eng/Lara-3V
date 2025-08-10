d514b5f4e3979e4830727d970f93a3933099ef31
malive939-eng <malive939@gmail.com>
2025-08-10 17:53:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "eIZRWT7J#ECDOG15OEgWYHCxB-8EJCJgwfTZ-YG2AnZ4yDXLB9Ok", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "GZZFGK4B#lV8vZg2KTI_Z-La_Ma4TtGNI8reVp0D1n2WtKfiV9jA", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94740237746",
 DEV: process.env.DEV || "94779062397",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
